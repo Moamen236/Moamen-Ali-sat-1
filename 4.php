55f283a933c6fe29da978d293554cddeca666982
Moamen236 <Moamen236@users.noreply.github.com>
2021-02-26 19:24:06 +0200

    Update 4.php

--- 4.php
+++ 4.php
@@ -13,42 +13,24 @@
 <body>
 
 
-    <div class="container">
+
+    <div class="container mt-5">
         <table class="table table-bordered">
+
             <?php
-            // $arr = [1,2,3,4,5,6,7,8,9,10,11,12];
-            // for($j = 0; $j < count($arr); $j++){
-            //     $arrNum = $arr[$j];
-            // }
-            // echo $arrNum ;
-            
+
             $arr = [1,2,3,4,5,6,7,8,9,10,11,12];
-            for($i = 1; $i <= 12; $i++){
-                for($j = 0; $j < count($arr); $j++){
-                    // $arrNum = $arr[$j];
-                    echo $arr[$j];
-                    $result = $arr[$j] * $i;
+            
+            for($j = 1; $j <= count($arr); $j++){
+                echo ' <tr>';
+                
+                for($i = 1; $i <= count($arr); $i++){
+                    $result = $j * $i ;
+                    echo "<td>$j x $i = $result</td>";
                 }
-                // echo $arrNum ;
-                // $result = 2 * $i;
                 
-                
-                echo "<tr>
-                        <td>1 x $i = $result</td>
-                        <td>2 x $i = $result</td>
-                        <td>3 x $i = $result</td>
-                        <td>4 x $i = $result</td>
-                        <td>5 x $i = $result</td>
-                        <td>6 x $i = $result</td>
-                        <td>7 x $i = $result</td>
-                        <td>8 x $i = $result</td>
-                        <td>9 x $i = $result</td>
-                        <td>10 x $i = $result</td>
-                        <td>11 x $i = $result</td>
-                        <td>12 x $i = $result</td>
-                    </tr>";
-            }
-                
+                echo ' </tr>';
+            }  
             ?>
 
         </table>
